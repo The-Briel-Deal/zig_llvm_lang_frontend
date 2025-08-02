5cab7cebae4ccc8e34f5a8ef97e469c37a150c6b
Gabriel Ford <gabe@fordltc.net>
2025-08-02 15:25:38 -0400

    use stdout for logging

--- test_runner.zig
+++ test_runner.zig
@@ -11,8 +11,12 @@ const BRIGHT_RED_FOREGROUND = "\x1b[101m";
 pub fn main() !void {
     for (builtin.test_functions) |t| {
         var buf: [64]u8 = undefined;
-        var stdout_writer = std.fs.File.stdout().writer(&buf);
+        var stdout = std.fs.File.stdout();
+
         t.func() catch |err| {
+            try stdout.lock(.exclusive);
+            defer stdout.unlock();
+            var stdout_writer = stdout.writer(&buf);
             _ = try stdout_writer.interface.write(BRIGHT_RED_FOREGROUND);
             _ = try stdout_writer.interface.write("[fail]");
             _ = try stdout_writer.interface.write(DEFAULT_COLOR);
@@ -20,6 +24,9 @@ pub fn main() !void {
             try stdout_writer.interface.flush();
             continue;
         };
+        try stdout.lock(.exclusive);
+        defer stdout.unlock();
+        var stdout_writer = stdout.writer(&buf);
         _ = try stdout_writer.interface.write(BRIGHT_GREEN_FOREGROUND);
         _ = try stdout_writer.interface.write("[pass]");
         _ = try stdout_writer.interface.write(DEFAULT_COLOR);
@@ -34,15 +41,19 @@ fn logHandler(
     args: anytype,
 ) void {
     var buf: [64]u8 = undefined;
-    const stderr_writer = std.debug.lockStderrWriter(&buf);
-    defer std.debug.unlockStderrWriter();
-    _ = stderr_writer.write("  ") catch return;
-    _ = stderr_writer.write(BRIGHT_YELLOW_FOREGROUND) catch return;
-    stderr_writer.print("[{s}]", .{msg_level.asText()}) catch return;
-    _ = stderr_writer.write(DEFAULT_COLOR) catch return;
-    _ = stderr_writer.write(" ") catch return;
-    stderr_writer.print(format, args) catch return;
-    _ = stderr_writer.write("\n") catch return;
+    var stdout = std.fs.File.stdout();
+    stdout.lock(.exclusive) catch return;
+    defer stdout.unlock();
+    var stdout_writer = stdout.writer(&buf);
+
+    _ = stdout_writer.interface.write("  ") catch return;
+    _ = stdout_writer.interface.write(BRIGHT_YELLOW_FOREGROUND) catch return;
+    stdout_writer.interface.print("[{s}]", .{msg_level.asText()}) catch return;
+    _ = stdout_writer.interface.write(DEFAULT_COLOR) catch return;
+    _ = stdout_writer.interface.write(" ") catch return;
+    stdout_writer.interface.print(format, args) catch return;
+    _ = stdout_writer.interface.write("\n") catch return;
+    stdout_writer.interface.flush() catch return;
 }
 
 pub const std_options: std.Options = .{
